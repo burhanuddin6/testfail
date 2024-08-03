e23b2734a42defcbcdeabbd22c0430162cb6486f
Burhanuddin <98301121+burhanuddin6@users.noreply.github.com>
2024-08-03 15:48:05 +0500

    docker

--- backend/drf_backend/drf_backend/settings.py
+++ backend/drf_backend/drf_backend/settings.py
@@ -176,5 +176,5 @@ AUTH_USER_MODEL = 'tcms.MyUser'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/server_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
