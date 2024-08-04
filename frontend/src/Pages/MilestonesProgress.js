b404262918f9d0ca9b603641a92f0daddada7241
Farhan Ahmed <farhan.ahmed@Farhan-Ahmeds-MacBook-Pro.local>
2024-08-04 21:16:14 +0500

    Connected Routes of Activity + Progress + Defects Pages in TR_T&S

--- frontend/src/Pages/MilestonesProgress.js
+++ frontend/src/Pages/MilestonesProgress.js
@@ -1,26 +1,78 @@
 import "../styles/MilestonesProgress.css"
-import React, { useState } from 'react';
+import React from 'react';
 import Graph from "../components/OverviewGraph";
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 
-const Milestonesprogress = () => {
+const MilestonesProgress = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const testRunId = searchParams.get("testRunId") || "0"; 
+    const testRunName = searchParams.get("testRunName") || "Test Run";
+    const sourcePage = searchParams.get('source');
+
+    const suiteId = searchParams.get('suiteId') || '0';
+    const suiteName = searchParams.get('suite') || 'Suite Name';
+
     const handleEditMilestone = () => {
         navigate(`/add-milestone`, { state: { from: '/milestone-progress' , action: 'edit'} });
-      };
+    };
 
     return (
         <div className="progress-mainclass">
             <div className="progress-header">
-                <div className="progress-options">
-                    <a href="/milestone-status">Status</a>
-                    <a href="/milestone-activity">Activity</a>
-                    <a href="/milestone-progress">Progress</a>
-                    <a href="/milestone-defect">Defects</a>
+            <div className="activity-options">
+                    {sourcePage === 'TestRunTestsResults' || sourcePage === 'milestone-activity' || sourcePage === 'milestone-progress' || sourcePage === 'milestone-defect' ? (
+                        <>
+                            <a
+                                href={`/TestRunTestsResults?suiteId=${suiteId}&suite=${suiteName}&testRunId=${testRunId}&testRunName=${testRunName}`}
+                                
+                            >
+                                Tests & Results
+                            </a>
+                            <a
+                                href={`/milestone-activity?suiteId=${suiteId}&suite=${suiteName}&testRunId=${testRunId}&testRunName=${testRunName}&source=milestone-progress`}
+                                
+                            >
+                                Activity
+                            </a>
+                            <a
+                                href={`/milestone-progress?suiteId=${suiteId}&suite=${suiteName}&testRunId=${testRunId}&testRunName=${testRunName}&source=milestone-progress`}
+    
+                            >
+                                Progress
+                            </a>
+                            <a
+                                href={`/milestone-defect?suiteId=${suiteId}&suite=${suiteName}&testRunId=${testRunId}&testRunName=${testRunName}&source=milestone-progress`}
+                                
+                            >
+                                Defects
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <a href="/milestone-status" >
+                                Status
+                            </a>
+                            <a href="/milestone-activity" >
+                                Activity
+                            </a>
+                            <a href="/milestone-progress" >
+                                Progress
+                            </a>
+                            <a href="/milestone-defect" >
+                                Defects
+                            </a>
+                        </>
+                    )}
                 </div>
                
                 <div className="progress-controls">
-                    <h3> Milestone Name </h3>
+                    {sourcePage === 'TestRunTestsResults' || sourcePage === 'milestone-activity' || sourcePage === 'milestone-progress' || sourcePage === 'milestone-defect' ? (
+                        <h2>{`R${testRunId} - ${testRunName}`}</h2>
+                    ) : (
+                        <h2>Milestone Name</h2>   
+                    )}
                     <div className="progress-control-button">
                         <button onClick={handleEditMilestone}>Edit</button>
                     </div>
@@ -38,15 +90,15 @@ const Milestonesprogress = () => {
                 <div className="progress-details-legend">
                     <div className="progress-legend-item">
                         <div className="progress-legend-color" style={{ backgroundColor: 'green' }}></div>
-                        <span className="progress-legend-text"><strong>Remaining Tests</strong><br></br>52% of tests completed</span>
+                        <span className="progress-legend-text"><strong>Remaining Tests</strong><br />52% of tests completed</span>
                     </div>
                     <div className="progress-legend-item">
                         <div className="progress-legend-color" style={{ backgroundColor: 'red' }}></div>
-                        <span className="progress-legend-text"><strong>Remaining Effort</strong><br></br>Forecast not available</span>
+                        <span className="progress-legend-text"><strong>Remaining Effort</strong><br />Forecast not available</span>
                     </div>
                     <div className="progress-legend-item">
                         <div className="progress-legend-color" style={{ backgroundColor: 'blue' }}></div>
-                        <span className="progress-legend-text"><strong>Ideal Progress</strong><br></br>Forecast not available</span>
+                        <span className="progress-legend-text"><strong>Ideal Progress</strong><br />Forecast not available</span>
                     </div>
                 </div>
             </div>
@@ -66,18 +118,18 @@ const Milestonesprogress = () => {
                     </div>
                 </div>
 
-                <hr></hr>
+                <hr />
                 
                 <div className="progress-details-section">
                     <div className="progress-expected-date-info">
                         <p>This milestone was started <strong>9 weeks ago</strong> (5/29/2024)</p>
                     </div>
                     <div className="progress-expected-date">
-                        <p> Completed: 5% (205/3491) <br></br> Elapsed: 0h 0m <br></br> Tests / day: 3 <br></br> Hours / day: n/a </p>
+                        <p> Completed: 5% (205/3491) <br /> Elapsed: 0h 0m <br /> Tests / day: 3 <br /> Hours / day: n/a </p>
                     </div>
                 </div>
                 
-                <hr></hr>
+                <hr />
 
                 <div className="progress-details-section">
                     <table className="progress-table">
@@ -107,62 +159,63 @@ const Milestonesprogress = () => {
                 </div>
             </div>
 
-            <h3> Test Runs </h3>
-            <div className="progress-testrun-class">
-                
-                <div className="progress-testrun-details">
-                    <p>Test run name</p>
-                    <div className="progress-testrun-forecast">
-                        <p>Forecast</p>
+            {sourcePage !== 'TestRunTestsResults' && sourcePage !== 'milestone-activity' && sourcePage !== 'milestone-progress' && sourcePage !== 'milestone-defect' && (
+                <>
+                    <h3> Test Runs </h3>
+                    <div className="progress-testrun-class">
+                        <div className="progress-testrun-details">
+                            <p>Test run name</p>
+                            <div className="progress-testrun-forecast">
+                                <p>Forecast</p>
+                            </div>
+                        </div>
+                        <div className="progress-testrun-details">
+                            <p>Test run name</p>
+                            <div className="progress-testrun-forecast">
+                                <p>Forecast</p>
+                            </div>
+                        </div>
+                        <div className="progress-testrun-details">
+                            <p>Test run name</p>
+                            <div className="progress-testrun-forecast">
+                                <p>Forecast</p>
+                            </div>
+                        </div>
+                        <div className="progress-testrun-details">
+                            <p>Test run name</p>
+                            <div className="progress-testrun-forecast">
+                                <p>Forecast</p>
+                            </div>
+                        </div>
+                        <div className="progress-testrun-details">
+                            <p>Test run name</p>
+                            <div className="progress-testrun-forecast">
+                                <p>Forecast</p>
+                            </div>
+                        </div>
+                        <div className="progress-testrun-details">
+                            <p>Test run name</p>
+                            <div className="progress-testrun-forecast">
+                                <p>Forecast</p>
+                            </div>
+                        </div>
+                        <div className="progress-testrun-details">
+                            <p>Test run name</p>
+                            <div className="progress-testrun-forecast">
+                                <p>Forecast</p>
+                            </div>
+                        </div>
+                        <div className="progress-testrun-details">
+                            <p>Test run name</p>
+                            <div className="progress-testrun-forecast">
+                                <p>Forecast</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="progress-testrun-details">
-                    <p>Test run name</p>
-                    <div className="progress-testrun-forecast">
-                        <p>Forecast</p>
-                    </div>
-                </div>
-                <div className="progress-testrun-details">
-                    <p>Test run name</p>
-                    <div className="progress-testrun-forecast">
-                        <p>Forecast</p>
-                    </div>
-                </div>
-                <div className="progress-testrun-details">
-                    <p>Test run name</p>
-                    <div className="progress-testrun-forecast">
-                        <p>Forecast</p>
-                    </div>
-                </div>
-                <div className="progress-testrun-details">
-                    <p>Test run name</p>
-                    <div className="progress-testrun-forecast">
-                        <p>Forecast</p>
-                    </div>
-                </div>
-                <div className="progress-testrun-details">
-                    <p>Test run name</p>
-                    <div className="progress-testrun-forecast">
-                        <p>Forecast</p>
-                    </div>
-                </div>
-                <div className="progress-testrun-details">
-                    <p>Test run name</p>
-                    <div className="progress-testrun-forecast">
-                        <p>Forecast</p>
-                    </div>
-                </div>
-                <div className="progress-testrun-details">
-                    <p>Test run name</p>
-                    <div className="progress-testrun-forecast">
-                        <p>Forecast</p>
-                    </div>
-                </div>
-            </div>
-
+                </>
+            )}
         </div>
     );
 }
 
-export default Milestonesprogress;
-
+export default MilestonesProgress;
